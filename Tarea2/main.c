42583d9d467ef73a665c0145fdf111499a2c30db
Manuel Olguin <molguin@dcc.uchile.cl>
2015-11-01 18:57:17 -0300

    Started working on Tarea2.

---
+++ Tarea2/main.c
@@ -0,0 +1,5 @@
+// Created by Manuel Olguin on 01-11-15.
+// main is part of Tarea2, for the course CC4102.
+// This submission is developed together with Camila Romero.
+
+#include "main.h"
---
+++ Tarea2/main.h
@@ -0,0 +1,8 @@
+// Created by Manuel Olguin on 01-11-15.
+// main is part of Tarea2, for the course CC4102.
+// This submission is developed together with Camila Romero.
+
+#ifndef TAREA2_MAIN_H
+#define TAREA2_MAIN_H
+
+#endif //TAREA2_MAIN_H
